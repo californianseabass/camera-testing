2ffc2c2b48273ac0c92e76ec73b8b6f1fd477169
Eric Sebastian <sebastian@sidewalklabs.com>
2019-03-27 15:03:18 -0400

    save as jpg backend

--- server.py
+++ server.py
@@ -1,16 +1,26 @@
+from base64 import b64decode
+from io import BytesIO
+
 from aiohttp import web
+from PIL import Image
+
 
 async def analyze_photo_handler(request):
     # WARNING: don't do that if you plan to receive large files!
     data = await request.post()
     print(data['photo']);
 
-    photo = data['photo']
-    blob = photo.file
-    content = blob.read()
+    content = data['photo']
     print(content)
+    image = Image.open(BytesIO(b64decode(content)))
+    image.convert('RGB')
+    if image.mode in ('RGBA', 'LA'):
+        background = Image.new(image.mode[:-1], image.size, (255, 255, 255))
+        background.paste(image, image.split()[-1])
+        image = background
 
-    return web.json_response(data)
+    image.save('pil.jpg', 'JPEG', quality=80, optimize=True, progressive=True)
+    return web.json_response({'status': 'success'})
 
 app = web.Application()
 app.add_routes([web.post('/api/analyze', analyze_photo_handler)])
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,29 @@
 import React, { Fragment } from 'react';
 import { render } from 'react-dom';
 
+function blobToBase64(blob, callback) {
+    var reader = new FileReader();
+    reader.onload = function() {
+        var dataUrl = reader.result;
+        console.log(dataUrl);
+        var base64 = dataUrl.split(',')[1];
+        callback(base64);
+    };
+    reader.readAsDataURL(blob);
+};
+
+async function sendBlob(blob: Blob): Promise<void> {
+    const body = new FormData();
+    body.append('photo', blob);
+    const response = await fetch('/api/analyze', {
+        method: 'POST',
+        body
+    })
+}
+
 // https://developer.mozilla.org/en-US/docs/Web/API/ImageCapture
 // https://developer.mozilla.org/en-US/docs/Web/API/ImageBitmap
 // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/drawImage
-
 async function getCamera() {
     const player: HTMLVideoElement = document.getElementById('player') as HTMLVideoElement;
 
@@ -18,18 +37,14 @@ async function getCamera() {
     const saveNewScreenShot = async () => {
         context.drawImage(player, 0, 0, canvas.width, canvas.height);
         const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-        const body = new FormData();
-        body.append('photo', new Blob([imageData.data]));
-        const response = await fetch('/api/analyze', {
-            method: 'POST',
-            body
+        const blob = new Blob([imageData.data]);
+        canvas.toBlob((b) => {
+            blobToBase64(b, sendBlob);
         })
     }
-
-
     document.getElementById("analyze-button").addEventListener("click", saveNewScreenShot, false);
 }
 
 getCamera().then(() => {
     console.log('done')
-})
+});
